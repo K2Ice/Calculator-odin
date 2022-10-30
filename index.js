405d0bf6a0c30d0aed88a3f7872ba26ac385379c
Konrad <k.korzeniewski94@gmail.com>
2022-10-30 22:30:24 +0100

    Create operate function to handle each calculator operation

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const ADD = "add";
+const SUBTRACT = "subtract"
+const MULTIPLY = "multiply";
+const DIVIDE = "divide";
+
 const add = (operand1, operand2)=>{
   //prevent from adding two strings
   operand1 = +operand1;
@@ -13,4 +18,19 @@ const multiply = (operand1, operand2)=>{
 }
 const divide = (operand1, operand2)=>{
   return operand1 / operand2;
+}
+
+const operate = (type, operand1, operand2) =>{
+  switch(type){
+    case ADD:
+      return add(operand1, operand2);
+    case SUBTRACT:
+      return subtract(operand1, operand2)
+    case MULTIPLY:
+      return multiply(operand1, operand2)
+    case DIVIDE:
+      return divide(operand1, operand2)
+    default:
+      throw new Error('No such type of function: '  + type)      
+  }
 }
