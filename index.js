eb6f79d796a1b3b2e885ed8f0b07629bcd9d2820
Konrad <k.korzeniewski94@gmail.com>
2022-10-31 03:00:19 +0100

    Create calculator logic, clean function and update screen function

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const SUBTRACT = "subtract"
 const MULTIPLY = "multiply";
 const DIVIDE = "divide";
 
+const signs = {
+  "add" : "+",
+  "subtract" : "-",
+  "multiply" : "*",
+  "divide" : "/"
+}
+
+const display = document.querySelector(".calculator-screen");
+const buttons = document.querySelectorAll('button')
+
+let displayValue = "";
+let operationType = ""
+let operand1 = null;
+let operand2 = null;
+
 const add = (operand1, operand2)=>{
   //prevent from adding two strings
   operand1 = +operand1;
@@ -34,3 +49,67 @@ const operate = (type, operand1, operand2) =>{
       throw new Error('No such type of function: '  + type)      
   }
 }
+
+function updateScreen(){
+  display.textContent = displayValue;
+}
+
+function clear(){
+  displayValue = "";
+  operationType = ""
+  operand1 = null;
+  operand2 = null;
+}
+
+function handleButtonClick(event){
+  const {btn} = event.target.dataset;
+  const type = isNaN(+btn) ? "operation" : "number";
+
+  console.log(type)
+
+  switch (type){
+    case "number":
+      if(operationType === "=") clear();
+      displayValue += btn;
+      if(operand1 && operationType){
+        operand2 += +btn
+      }
+      break;
+    case "operation":
+      if(btn === 'clear') clear();
+      else if(btn === 'equal'){
+        if(operand1 && operand2 && operationType){
+        displayValue = operate(operationType,operand1,operand2)
+        operationType = "="
+        operand1 = displayValue;
+        operand2 = null;
+        }
+      }
+      else if (operand1 && operand2 && operationType){
+        operand1 = operate(operationType,operand1,operand2 )
+        operand2 = null;
+        operationType = btn;
+        displayValue = operand1 + " " + signs[btn] + " ";
+      }
+      else if(operand1 && operationType && !operand2){
+        operationType = btn;
+        displayValue = operand1 + " " + signs[btn] + " ";
+      }
+      else if(operand1 === null && displayValue === "") return;
+      else if(!operand1 && !operationType) {
+        operationType = btn;
+        operand1 = +displayValue
+        displayValue += " " + signs[btn] + " ";
+      }else{
+        console.log('in')
+        operationType = btn;
+        displayValue = +operand1 + " " + signs[btn] + " ";
+      }
+      break;
+  }
+
+  updateScreen()
+  
+}
+
+buttons.forEach(btn=> btn.addEventListener('click', handleButtonClick));
